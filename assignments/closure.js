2e3c991f9fb2bccccc36ae04319cd54ec801fcb6
Sandy Jernigan <sandy.jernigan@gmail.com>
2019-04-20 12:20:09 -0400

    completed Challenge 1: Write your own closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,9 +1,30 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+// One aspect of a closure, closures are a tool for passing data to the next interaction (or function).
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
+function animals() {
+  const animalsIntro = "There are many species in the animal kingdom."
+  const kingdom = "animal"
+  console.log(animalsIntro);
 
+  function mammals() {
+    const mammalsIntro = "There are around 5000 species of mammals."
+    const group = "mammal"
+    console.log(mammalsIntro);
+
+    function canines() {
+      const species = "canine"
+      console.log(`A Gray wold is a ${species} in the ${group} group of the ${kingdom} kingdom.`);
+    }
+
+    canines();
+  } // mammals
+
+  mammals(); 
+} // animals
+
+animals();
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
