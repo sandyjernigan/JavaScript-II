3415a548f3568bed8240bf6037ba0c4686769add
Sandy Jernigan <sandy.jernigan@gmail.com>
2019-04-20 15:10:20 -0400

    STRETCH PROBLEM for callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -59,5 +59,7 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
-  
+  let uniqueValues = [...new Set(array)];
+  return cb(uniqueValues);
 }
+
